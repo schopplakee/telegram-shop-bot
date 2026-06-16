759e81f4ae1483881153c713923911e7ce03402a
roozib3h <roozbehtavakoli55@gmail.com>
2026-06-17 01:02:23 +0330

    Admin Country Lists Commite

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -1,34 +1,35 @@
 const sessionManager = require("../sessions/sessionManager");
+const countryService = require("../services/countryService");
 
 const SESSION_MODULES = require("../constants/sessionModules");
 const SESSION_STEPS = require("../constants/sessionSteps");
 
 module.exports = {
+  async addCountry(ctx) {
+    await sessionManager.start(
+      ctx.from.id,
 
-    async addCountry(ctx) {
+      SESSION_MODULES.COUNTRY,
 
-        await sessionManager.start(
+      SESSION_STEPS.NAME,
+    );
 
-            ctx.from.id,
+    return ctx.reply("🌍 لطفاً نام کشور را وارد کنید.");
+  },
 
-            SESSION_MODULES.COUNTRY,
+  async listCountries(ctx) {
+    const countries = await countryService.getCountries();
 
-            SESSION_STEPS.NAME
-
-        );
-
-        return ctx.reply(
-            "🌍 لطفاً نام کشور را وارد کنید."
-        );
-
-    },
-
-    async listCountries(ctx) {
-
-        return ctx.reply(
-            "📋 هنوز کشوری ثبت نشده است."
-        );
-
+    if (!countries.length) {
+      return ctx.reply("❌ هنوز کشوری ثبت نشده است.");
     }
 
+    let message = "🌍 لیست کشورها\n\n";
+
+    countries.forEach((country, index) => {
+      message += `${index + 1}. ${country.flag} ${country.name} (${country.code})\n`;
+    });
+
+    return ctx.reply(message);
+  },
 };
